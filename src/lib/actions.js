ec3b67808193bb77627a4a2ded2ef55728c620c1
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2023-11-29 11:14:46 +0100

    ...

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -6,29 +6,29 @@ import { cookies, headers } from 'next/headers'
 import { revalidatePath } from 'next/cache'
 
 
-export async function login (formData) {
+export async function login(formData) {
     const email = await formData.get('email')
     const password = await formData.get('password')
 
-    const encontrado = users.filter ( (u) => (u.email === email && u.password === password ))
+    const encontrado = users.filter((u) => (u.email === email && u.password === password))
     // console.log(encontrado);
 
-    if (encontrado.length > 0 ) {
+    if (encontrado.length > 0) {
         cookies().set('usuario',  email) 
         revalidatePath('/dashboard');
-        redirect('/dashboard')   
+        redirect('/dashboard')
     }
     else {
-        redirect('/')   
+        redirect('/')
     }
 }
 
 
-export async function logout () {   
+export async function logout() {
     const origin = headers().get('origin');
     // for (const [key,value] of headers().entries() ) {
     //     console.log(key, value)
     // }
     cookies().delete('usuario')
-    redirect(`${origin}/`) 
+    redirect(`${origin}/`)
 }
